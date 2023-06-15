257882197aa3f494ca03be1499855ec0cab8edc2
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-15 10:12:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-//your code here
-
-   class OutOfRangeError extends Error {
+class OutOfRangeError extends Error {
   constructor() {
     super();
     this.name = this.constructor.name;
-    this.message = "Expression should only consist of integers and +-/* characters and not <arg>";
+    this.message = 'Expression should only consist of integers and +-/* characters';
   }
 }
 
@@ -12,22 +10,44 @@ class InvalidExprError extends Error {
   constructor() {
     super();
     this.name = this.constructor.name;
-    this.message = "Expression should not have an invalid combination of expression";
+    this.message = 'Expression should not have an invalid combination of expression';
   }
 }
 
 function evalString(expression) {
   try {
-    if (/[\+\-\*\/]{2,}/.test(expression)) {
+    if (/(\+{2,}|\-{2,}|\*{2,}|\/{2,})/.test(expression)) {
       throw new InvalidExprError();
     }
-
-    if (/^[\+\*\/].*$/.test(expression)) {
-      throw new SyntaxError("Expression should not start with an invalid operator");
+    
+    if (/^[\/*+]/.test(expression)) {
+      throw new SyntaxError('Expression should not start with an invalid operator');
     }
-
-    if (/.*[\+\*\/\-]$/.test(expression)) {
-      throw new SyntaxError("Expression should not end with an invalid operator");
+    
+    if (/[\/*+\-]$/.test(expression)) {
+      throw new SyntaxError('Expression should not end with an invalid operator');
     }
+    
+    // Rest of your evaluation logic goes here
+    
+    return 'Valid expression'; // Placeholder return value for demonstration
+    
+  } catch (error) {
+    if (error instanceof OutOfRangeError || error instanceof InvalidExprError) {
+      throw error;
+    } else {
+      throw new OutOfRangeError();
+    }
+  }
+}
+
+// Example usage:
+try {
+  evalString('2+3'); // Valid expression
+  evalString('2+3+'); // Throws SyntaxError: Expression should not end with an invalid operator
+  evalString('2++3'); // Throws InvalidExprError: Expression should not have an invalid combination of expression
+} catch (error) {
+  console.log(error.name + ': ' + error.message);
+}
 
    
